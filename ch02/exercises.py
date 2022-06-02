454119102fa8e94c95b8d4d71642df6e4a465460
mlsimpson <maui@threv.net>
2022-06-02 13:20:29 -0400

    added exercises

--- ch02/exercises.py
+++ ch02/exercises.py
@@ -9,9 +9,79 @@
 
 for row in range(1, 5):
     message = ""
-    for space in range(row - 1):
+    for _ in range(row - 1):
         message += ' '
-    for hash in range(8 - 2 * (row - 1)):
+    for _ in range(8 - 2 * (row - 1)):
+        message += '#'
+    print(message)
+
+print('\n')
+
+# 2-2
+# or how about:
+#    ##
+#   ####
+#  ######
+# ########
+# ########
+#  ######
+#   ####
+#    ##
+
+# top half
+for row in range(1, 5):
+    message = ""
+    for _ in range(abs(4 - row)):
+        message += ' '
+    for _ in range(2 * row):
+        message += '#'
+    print(message)
+
+# bottom half
+for row in range(1, 5):
+    message = ""
+    for _ in range(row - 1):
+        message += ' '
+    for _ in range(8 - 2 * (row - 1)):
+        message += '#'
+    print(message)
+
+print('\n')
+
+# 2-3
+# Here’s an especially tricky one:
+# #            #
+#  ##        ##
+#   ###    ###
+#    ########
+#    ########
+#   ###    ###
+#  ##        ##
+# #            #
+
+# top half
+for row in range(1, 5):
+    message = ""
+    for _ in range(row - 1):
+        message += ' '
+    for _ in range(row):
+        message += '#'
+    for _ in range(4 * (4 - row)):
+        message += ' '
+    for _ in range(row):
+        message += '#'
+    print(message)
+
+# bottom half
+for row in range(1, 5):
+    message = ""
+    for _ in range(4 - row):
+        message += ' '
+    for _ in range(5 - row):
+        message += '#'
+    for _ in range(4 * (row - 1)):
+        message += ' '
+    for _ in range(5 - row):
         message += '#'
     print(message)
 
